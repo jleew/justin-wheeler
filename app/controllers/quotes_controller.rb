9a4c20705ffa78f302637d2b33ef177075328342
Justin <jleew357@gmail.com>
2019-08-20 01:25:31 +0000

    ureka

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -6,11 +6,7 @@ class QuotesController < ApplicationController
     @quote = Quote.order("RANDOM()").first 
   end
 
-  def new
-
-    @quote = Quote.new
-
-  end
+  
 
  def create
     @quote = Quote.create(quote_params)
