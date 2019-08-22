37513963fe61cdb476c37ceccaad4b2aafd3ff2f
Justin <jleew357@gmail.com>
2019-08-22 00:21:12 +0000

    done

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -175,5 +175,10 @@ SimpleForm.setup do |config|
 
   # Defines validation classes to the input_field. By default it's nil.
   # config.input_field_valid_class = 'is-valid'
+  
   # config.input_field_error_class = 'is-invalid'
+
+
 end
+
+#config.label_text = lambda { |label, required| "#{label}" }
